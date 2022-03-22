ef4dd77d1dde18128a0ae33e56cd108fffec9efc
SylvanBrocard <sylvan.brocard@gmail.com>
2022-03-22 10:29:06 +0100

    benchmark script change

--- benchmarks/scripts/simple_run.py
+++ benchmarks/scripts/simple_run.py
@@ -12,11 +12,11 @@ from dpu_kmeans import DIMM_data, KMeans, _dimm
 nfeatures = 8
 min_nclusters = 15
 max_nclusters = 15
-loops = 10
+loops = 1
 verbose = False
 tol = 1e-4
 
-npoints_str = "1e5"
+npoints_str = "1e4"
 n_cluster_set = list(range(min_nclusters, max_nclusters + 1))
 times = []
 inner_times = []
@@ -61,6 +61,6 @@ times.append(n_clusters_time.copy())
 inner_times.append(n_clusters_inner_time.copy())
 iter.append(n_clusters_iter.copy())
 
-if min_nclusters == max_nclusters:
-    print("centroids:")
-    print(centroids)
+# if min_nclusters == max_nclusters:
+#     print("centroids:")
+#     print(centroids)
