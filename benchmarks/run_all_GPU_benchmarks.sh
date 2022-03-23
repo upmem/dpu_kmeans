c2a14e72a3486380b20884c9426599bc76cc063a
SylvanBrocard <sylvan.brocard@gmail.com>
2022-03-23 14:58:46 +0100

    more verbose benchmark script

--- benchmarks/run_all_GPU_benchmarks.sh
+++ benchmarks/run_all_GPU_benchmarks.sh
@@ -1,9 +1,13 @@
 cd weak_scaling || exit
+echo "Running weak scaling benchmark"
 python GPU.py
 cd ../strong_scaling || exit
+echo "Running strong scaling benchmark"
 python GPU.py
 cd ../higgs || exit
 if [[ ! -f "./data/higgs.pq" ]]; then
+    echo "Downloading Higgs dataset"
     sh ./download_dataset.sh
 fi
+echo "Running Higgs benchmark"
 python GPU.py
