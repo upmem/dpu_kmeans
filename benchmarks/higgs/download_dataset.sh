3ffd0f6cd71328df04a8e0693738bc30236c137e
SylvanBrocard <sylvan.brocard@gmail.com>
2022-03-23 15:46:45 +0100

    more verbose download script

--- benchmarks/higgs/download_dataset.sh
+++ benchmarks/higgs/download_dataset.sh
@@ -1,6 +1,8 @@
 mkdir -p data
 cd data || exit
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz
+echo "unzipping data"
 gunzip HIGGS.csv
 cd ..
+echo "converting to parquet format"
 python convert_to_pq.py
