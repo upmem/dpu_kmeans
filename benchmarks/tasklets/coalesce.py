8f6fdc545b08a0305db4fa50d34c1165cb381b34
SylvanBrocard <sylvan.brocard@gmail.com>
2022-03-30 10:48:58 +0200

    tasklets benchmarks

---
+++ benchmarks/tasklets/coalesce.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import glob
+
+import pandas as pd
+
+all_files = glob.glob("*.pkl")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_pickle(filename)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=False)
+frame.index.name = "tasklets"
+frame.sort_index(inplace=True)
+
+frame.to_csv("tasklets.csv")
+frame.to_pickle("tasklets.pkl")
