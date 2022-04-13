d1b97d1609fe8e09ec8ca1a7537911bbb92f600b
SylvanBrocard <sylvan.brocard@gmail.com>
2022-04-13 16:26:37 +0200

    reworked functional tests

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,79 +1,76 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Module for automated functional testing"""
+"""Module for automated functional testing
+Tests the K-Means at the service level"""
 
 # Author: Sylvan Brocard <sbrocard@upmem.com>
 # License: MIT
 
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
 import numpy as np
-from dpu_kmeans import KMeans as DPUKMeans, _dimm
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+from sklearn.metrics import adjusted_rand_score
+
+from dpu_kmeans import KMeans as DPUKMeans
+from dpu_kmeans import _dimm
 
 N_CLUSTERS = 15
 
 
 def test_clustering_dpu_then_cpu():
-    """Tests the K-Means at the service level"""
+    """Make clustering on DPUs and then on CPU, and compare the results"""
 
     # Generating data
     data = make_blobs(int(1e4), 8, centers=N_CLUSTERS, random_state=42)[0].astype(
         np.float32
     )
 
-    # Clustering with DPUs
-    _dimm.set_n_dpu(4)
-
     init = data[:N_CLUSTERS]
 
+    # Clustering with DPUs
+    _dimm.set_n_dpu(4)
     dpu_kmeans = DPUKMeans(N_CLUSTERS, init=init, n_init=1, verbose=False)
     dpu_kmeans.fit(data)
-    dpu_centroids = dpu_kmeans.cluster_centers_
 
     # Clustering with CPU
     kmeans = KMeans(N_CLUSTERS, init=init, n_init=1, algorithm="full")
     kmeans.fit(data)
-    centroids = kmeans.cluster_centers_
 
     # Comparison
-    relative_distance = np.linalg.norm(dpu_centroids - centroids) / np.linalg.norm(
-        centroids
-    )
-    assert relative_distance < 1e-3
+    rand_score = adjusted_rand_score(dpu_kmeans.labels_, kmeans.labels_)
+
+    assert rand_score > 1 - 1e-2
     assert kmeans.n_iter_ * 2 / 3 < dpu_kmeans.n_iter_ < kmeans.n_iter_ * 1.5
 
 
 def test_clustering_cpu_then_dpu():
-    """Tests the K-Means at the service level"""
+    """Make clustering on CPUs and then on DPU, and compare the results"""
 
     # Generating data
     data = make_blobs(int(1e4), 8, centers=N_CLUSTERS, random_state=42)[0].astype(
         np.float32
     )
 
-    # Clustering with CPU
     init = data[:N_CLUSTERS]
+
+    # Clustering with CPU
     kmeans = KMeans(N_CLUSTERS, init=init, n_init=1, algorithm="full")
     kmeans.fit(data)
-    centroids = kmeans.cluster_centers_
 
     # Clustering with DPUs
     _dimm.set_n_dpu(4)
-
     dpu_kmeans = DPUKMeans(N_CLUSTERS, init=init, n_init=1, verbose=False)
     dpu_kmeans.fit(data)
-    dpu_centroids = dpu_kmeans.cluster_centers_
 
     # Comparison
-    relative_distance = np.linalg.norm(dpu_centroids - centroids) / np.linalg.norm(
-        centroids
-    )
-    assert relative_distance < 1e-3
+    rand_score = adjusted_rand_score(dpu_kmeans.labels_, kmeans.labels_)
+
+    assert rand_score > 1 - 1e-2
     assert kmeans.n_iter_ * 2 / 3 < dpu_kmeans.n_iter_ < kmeans.n_iter_ * 1.5
 
 
 def test_clustering_dpu_then_dpu():
-    """Tests the K-Means at the service level"""
+    """Make clustering on DPU twice, and compare the results"""
 
     # Generating data
     data = make_blobs(int(1e4), 8, centers=N_CLUSTERS, random_state=42)[0].astype(
@@ -81,25 +78,25 @@ def test_clustering_dpu_then_dpu():
     )
     data_copy = data.copy()
 
+    init = data[:N_CLUSTERS]
+
     # Clustering with DPUs
     _dimm.set_n_dpu(4)
 
-    init = data[:N_CLUSTERS]
     dpu_kmeans = DPUKMeans(N_CLUSTERS, init=init, n_init=1, verbose=False)
     dpu_kmeans.fit(data)
     n_iter_1 = dpu_kmeans.n_iter_
-    dpu_centroids_1 = dpu_kmeans.cluster_centers_
+    dpu_labels_1 = dpu_kmeans.labels_
 
     dpu_kmeans = DPUKMeans(N_CLUSTERS, init=init, n_init=1, verbose=False)
     dpu_kmeans.fit(data_copy)
-    dpu_centroids_2 = dpu_kmeans.cluster_centers_
     n_iter_2 = dpu_kmeans.n_iter_
+    dpu_labels_2 = dpu_kmeans.labels_
 
     # Comparison
-    relative_distance = np.linalg.norm(
-        dpu_centroids_1 - dpu_centroids_2
-    ) / np.linalg.norm(dpu_centroids_1)
-    assert relative_distance < 1e-3
+    rand_score = adjusted_rand_score(dpu_labels_1, dpu_labels_2)
+
+    assert rand_score > 1 - 1e-4
     assert n_iter_1 == n_iter_2
 
 
