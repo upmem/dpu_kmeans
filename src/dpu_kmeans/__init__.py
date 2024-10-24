a08a9cc991cfb6365d513840ecb00debd7e792c3
SylvanBrocard <sylvan.brocard@gmail.com>
2024-10-24 18:19:19 +0200

    `from dpu_kmeans import *` now only import KMeans

--- src/dpu_kmeans/__init__.py
+++ src/dpu_kmeans/__init__.py
@@ -4,9 +4,12 @@
 # License: MIT
 
 from ._checksum import test_checksum
-from ._core import __doc__, __version__, add, subtract
+from ._core import __version__, add, subtract
 from ._kmeans import KMeans
 
 
 def add2(x: int, y: int) -> int:
     return add(x, y)
+
+
+__all__ = ["KMeans"]
