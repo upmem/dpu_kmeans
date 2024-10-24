059fb4da371f4a6bec9c305140ad6e9a794e6d6e
SylvanBrocard <sylvan.brocard@gmail.com>
2024-10-24 18:20:41 +0200

    ruff format

--- src/dpu_kmeans/perf.py
+++ src/dpu_kmeans/perf.py
@@ -12,7 +12,14 @@ alloc_dealloc_timer = 0
 
 
 def reset_timers():
-    global host_reduction_timer, host_quantize_timer, host_unquantize_timer, host_average_timer, host_c_timer, alloc_dealloc_timer, host_reduction_timer
+    global \
+        host_reduction_timer, \
+        host_quantize_timer, \
+        host_unquantize_timer, \
+        host_average_timer, \
+        host_c_timer, \
+        alloc_dealloc_timer, \
+        host_reduction_timer
     host_quantize_timer = 0
     host_unquantize_timer = 0
     host_average_timer = 0
