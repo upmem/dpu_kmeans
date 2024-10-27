6379332fd6ba40cb142f4ec419c3c08fefeece9c
SylvanBrocard <sylvan.brocard@gmail.com>
2024-10-27 18:56:34 +0100

    refactors:  - naming convetions  - static_cast over C style cast  - trailing function types  - use const ref instead of copying python arrays

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
  * @copyright 2021 UPMEM
  */
 
-#include <cstddef>
-#include <cstdint>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
+
+#include <cstddef>
+#include <cstdint>
 
 extern "C" {
 #include <dpu.h>
@@ -21,9 +22,9 @@ extern "C" {
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) { return i + j; }
+[[nodiscard]] constexpr auto add(int i, int j) -> int { return i + j; }
 
-extern "C" int checksum(char *);
+extern "C" auto checksum(char *) -> int;
 
 namespace py = pybind11;
 
@@ -35,40 +36,46 @@ namespace py = pybind11;
  */
 class Container {
  private:
-  Params p{}; /**< Struct containing various algorithm parameters. */
-  int_feature **features_int{};      /**< The discretized dataset features as a
+  Params p_{}; /**< Struct containing various algorithm parameters. */
+  int_feature **features_int_{};      /**< The discretized dataset features as a
                                          jagged array. */
-  int64_t *partial_sums_per_dpu{};   /**< Iteration buffer to read feature sums
+  int64_t *partial_sums_per_dpu_{};   /**< Iteration buffer to read feature sums
                                       from the DPUs. */
-  int *points_in_clusters_per_dpu{}; /**< Iteration buffer to read cluster
+  int *points_in_clusters_per_dpu_{}; /**< Iteration buffer to read cluster
                                       counts from the DPUs. */
-  uint64_t *inertia_per_dpu{};  /**< Iteration buffer to read inertia from the
+  uint64_t *inertia_per_dpu_{};  /**< Iteration buffer to read inertia from the
                                     DPUs. */
-  bool host_memory_allocated{}; /**< Whether the iteration buffers have been
+  bool host_memory_allocated_{}; /**< Whether the iteration buffers have been
                                  allocated. */
 
  public:
   /**
    * @brief Allocates all DPUs.
    */
-  void allocate() { ::allocate_dpus(&p); }
+  void allocate() { ::allocate_dpus(&p_); }
 
-  size_t get_ndpu() { return p.ndpu; }
+  [[nodiscard]] auto get_ndpu() const -> size_t { return p_.ndpu; }
 
-  void set_ndpu(uint32_t ndpu) { p.ndpu = ndpu; }
+  void set_ndpu(uint32_t ndpu) { p_.ndpu = ndpu; }
 
-  void reset_timer() { p.time_seconds = 0.0; }
+  void reset_timer() { p_.time_seconds = 0.0; }
 
-  double get_dpu_run_time() { return p.time_seconds; }
-  double get_cpu_pim_time() { return p.cpu_pim_time; }
-  double get_pim_cpu_time() { return p.pim_cpu_time; }
+  [[nodiscard]] auto get_dpu_run_time() const -> double {
+    return p_.time_seconds;
+  }
+  [[nodiscard]] auto get_cpu_pim_time() const -> double {
+    return p_.cpu_pim_time;
+  }
+  [[nodiscard]] auto get_pim_cpu_time() const -> double {
+    return p_.pim_cpu_time;
+  }
 
   /**
    * @brief Loads binary into the DPUs
    *
    * @param DPU_BINARY Path to the binary.
    */
-  void load_kernel(const char *DPU_BINARY) { ::load_kernel(&p, DPU_BINARY); }
+  void load_kernel(const char *DPU_BINARY) { ::load_kernel(&p_, DPU_BINARY); }
 
   /**
    * @brief Loads data into the DPUs from a python array
@@ -78,16 +85,18 @@ class Container {
    * @param nfeatures Number of features.
    * @param threshold Parameter to declare convergence.
    */
-  void load_array_data(py::array_t<int_feature> data_int, uint64_t npoints,
-                       int nfeatures) {
-    int_feature *data_int_ptr = (int_feature *)data_int.request().ptr;
+  void load_array_data(const py::array_t<int_feature> &data_int,
+                       uint64_t npoints, int nfeatures) {
+    int_feature *data_int_ptr =
+        static_cast<int_feature *>(data_int.request().ptr);
 
-    p.npoints = npoints;
-    p.nfeatures = nfeatures;
-    p.npadded = ((p.npoints + 8 * p.ndpu - 1) / (8 * p.ndpu)) * 8 * p.ndpu;
-    p.npointperdpu = p.npadded / p.ndpu;
+    p_.npoints = npoints;
+    p_.nfeatures = nfeatures;
+    p_.npadded = ((p_.npoints + 8 * p_.ndpu - 1) / (8 * p_.ndpu)) * 8 * p_.ndpu;
+    p_.npointperdpu = p_.npadded / p_.ndpu;
 
-    build_jagged_array_int(p.npadded, p.nfeatures, data_int_ptr, &features_int);
+    build_jagged_array_int(p_.npadded, p_.nfeatures, data_int_ptr,
+                           &features_int_);
     transfer_data();
   }
 
@@ -97,55 +106,58 @@ class Container {
    * @param nclusters Number of clusters.
    */
   void load_nclusters(unsigned int nclusters) {
-    p.nclusters = nclusters;
+    p_.nclusters = nclusters;
 
-    broadcastNumberOfClusters(&p, nclusters);
-    allocateHostMemory();
+    broadcastNumberOfClusters(&p_, nclusters);
+    allocate_host_memory();
   }
 
   /**
    * @brief Allocates host iteration buffers.
    *
    */
-  void allocateHostMemory() {
-    if (host_memory_allocated) deallocateHostMemory();
+  void allocate_host_memory() {
+    if (host_memory_allocated_) {
+      deallocate_host_memory();
+    }
 
     /* allocate array to read coordinates sums from the DPUs */
-    partial_sums_per_dpu = (int64_t *)malloc(
-        p.nclusters * p.ndpu * p.nfeatures * sizeof(*partial_sums_per_dpu));
+    partial_sums_per_dpu_ = static_cast<int64_t *>(malloc(
+        p_.nclusters * p_.ndpu * p_.nfeatures * sizeof(*partial_sums_per_dpu_)));
 
     /* allocate array to read clusters counts from the DPUs */
-    size_t count_in_8bytes = 8 / sizeof(*points_in_clusters_per_dpu);
+    size_t count_in_8bytes = 8 / sizeof(*points_in_clusters_per_dpu_);
     size_t nclusters_aligned =
-        ((p.nclusters + count_in_8bytes - 1) / count_in_8bytes) *
+        ((p_.nclusters + count_in_8bytes - 1) / count_in_8bytes) *
         count_in_8bytes;
-    points_in_clusters_per_dpu = (int *)malloc(
-        p.ndpu * nclusters_aligned * sizeof(*points_in_clusters_per_dpu));
+    points_in_clusters_per_dpu_ = static_cast<int *>(malloc(
+        p_.ndpu * nclusters_aligned * sizeof(*points_in_clusters_per_dpu_)));
 
     /* allocate array to read inertia from the DPUs */
-    inertia_per_dpu = (uint64_t *)malloc(p.ndpu * sizeof(*inertia_per_dpu));
+    inertia_per_dpu_ =
+        static_cast<uint64_t *>(malloc(p_.ndpu * sizeof(*inertia_per_dpu_)));
 
-    host_memory_allocated = true;
+    host_memory_allocated_ = true;
   }
 
   /**
    * @brief Frees the host iteration buffers.
    *
    */
-  void deallocateHostMemory() {
-    free(partial_sums_per_dpu);
-    free(points_in_clusters_per_dpu);
-    free(inertia_per_dpu);
+  void deallocate_host_memory() {
+    free(partial_sums_per_dpu_);
+    free(points_in_clusters_per_dpu_);
+    free(inertia_per_dpu_);
 
-    host_memory_allocated = false;
+    host_memory_allocated_ = false;
   }
 
   /**
    * @brief Preprocesses and transfers quantized data to the DPUs.
    */
   void transfer_data() {
-    populateDpu(&p, features_int);
-    broadcastParameters(&p);
+    populateDpu(&p_, features_int_);
+    broadcastParameters(&p_);
 #ifdef FLT_REDUCE
     allocateMembershipTable(&p);
 #endif
@@ -157,7 +169,7 @@ class Container {
    * managed by Python.
    */
   void free_data() {
-    free(features_int);
+    free(features_int_);
 #ifdef FLT_REDUCE
     deallocateMembershipTable();
 #endif
@@ -166,7 +178,7 @@ class Container {
   /**
    * @brief Frees the DPUs
    */
-  void free_dpus() { ::free_dpus(&p); }
+  void free_dpus() { ::free_dpus(&p_); }
 
   /**
    * @brief Runs one iteration of the K-Means Lloyd algorithm.
@@ -177,15 +189,17 @@ class Container {
    * centroids (before division by number of points).
    * @param points_in_clusters [out] Counts of points per cluster.
    */
-  void lloyd_iter(py::array_t<int_feature> centers_old_int,
-                  py::array_t<int64_t> centers_new_int,
-                  py::array_t<int> points_in_clusters) {
-    int_feature *old_centers = (int_feature *)centers_old_int.request().ptr;
-    int64_t *new_centers = (int64_t *)centers_new_int.request().ptr;
-    int *new_centers_len = (int *)points_in_clusters.request().ptr;
+  void lloyd_iter(const py::array_t<int_feature> &centers_old_int,
+                  const py::array_t<int64_t> &centers_new_int,
+                  const py::array_t<int> &points_in_clusters) {
+    int_feature *old_centers =
+        static_cast<int_feature *>(centers_old_int.request().ptr);
+    int64_t *new_centers =
+        static_cast<int64_t *>(centers_new_int.request().ptr);
+    int *new_centers_len = static_cast<int *>(points_in_clusters.request().ptr);
 
-    lloydIter(&p, old_centers, new_centers, new_centers_len,
-              points_in_clusters_per_dpu, partial_sums_per_dpu);
+    lloydIter(&p_, old_centers, new_centers, new_centers_len,
+              points_in_clusters_per_dpu_, partial_sums_per_dpu_);
   }
 
   /**
@@ -195,13 +209,11 @@ class Container {
    * centroids.
    * @return uint64_t The inertia.
    */
-  uint64_t compute_inertia(py::array_t<int_feature> centers_old_int) {
-    int_feature *old_centers = (int_feature *)centers_old_int.request().ptr;
-    uint64_t inertia;
-
-    inertia = lloydIterWithInertia(&p, old_centers, inertia_per_dpu);
-
-    return inertia;
+  auto compute_inertia(const py::array_t<int_feature> &centers_old_int)
+      -> uint64_t {
+    int_feature *old_centers =
+        static_cast<int_feature *>(centers_old_int.request().ptr);
+    return lloydIterWithInertia(&p_, old_centers, inertia_per_dpu_);
   }
 };
 
@@ -235,8 +247,8 @@ PYBIND11_MODULE(_core, m) {
       .def("free_dpus", &Container::free_dpus)
       .def("lloyd_iter", &Container::lloyd_iter)
       .def("compute_inertia", &Container::compute_inertia)
-      .def("allocate_host_memory", &Container::allocateHostMemory)
-      .def("deallocate_host_memory", &Container::deallocateHostMemory)
+      .def("allocate_host_memory", &Container::allocate_host_memory)
+      .def("deallocate_host_memory", &Container::deallocate_host_memory)
       .def("reset_timer", &Container::reset_timer)
       .def("get_dpu_run_time", &Container::get_dpu_run_time)
       .def("get_cpu_pim_time", &Container::get_cpu_pim_time)
